c0649b98ed022c3cae37f9befb1b7e12e8bf904c
giovanni antino <giovanni.antino@takamaka.io>
2023-01-11 15:43:03 +0100

    Added a stop clause in case an audit fails

--- src/main/java/io/takamaka/wallet/utils/TkmWallet.java
+++ src/main/java/io/takamaka/wallet/utils/TkmWallet.java
@@ -30,7 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 public class TkmWallet {
 
     /**
-     * It returns the Chrypted envelope used later in TKM chain
+     * It returns the signed envelope used later in TKM chain
      *
      * @param itb Java Bean Class - it contains all the transaction elements
      * needed
@@ -43,7 +43,27 @@ public class TkmWallet {
      */
     public static TransactionBean createGenericTransaction(InternalTransactionBean itb, InstanceWalletKeystoreInterface iwk, int signKey) throws TransactionCanNotBeCreatedException {
         try {
-            //todo verify itb transaction
+            /**
+             * The method createGenericTransaction creates a new TransactionBean
+             * object from the provided InternalTransactionBean object,
+             * InstanceWalletKeystoreInterface object and the index of the
+             * signing key. The InternalTransactionBean object is used to set
+             * the message field of the newly created TransactionBean object,
+             * the hash of the InternalTransactionBean is calculated and set as
+             * well. Then a random seed is generated for this transaction. The
+             * publicKey field is set by calling the getPublicKeyAtIndexURL64
+             * method of InstanceWalletKeystoreInterface with the given signKey
+             * as an input. The walletCypher field is set by calling the
+             * getWalletCypher method of InstanceWalletKeystoreInterface. The
+             * method then creates a TkmCypherBean and signs the message by
+             * calling the appropriate sign method from the TkmCypherProvider
+             * class based on the walletCypher. The signature and the cypher
+             * type is set on the TransactionBean object. The method then check
+             * the valid syntax of the transaction and returns it. If any
+             * exception occurs it will throw
+             * TransactionCanNotBeCreatedException with the caught exception as
+             * its parameter.
+             */
             itb.setTransactionHash(TkmTextUtils.internalTransactionBeanHash(itb));
             TransactionBean tb = new TransactionBean();
             tb.setRandomSeed(TkmTextUtils.generateWalletRandomString());
@@ -90,6 +110,26 @@ public class TkmWallet {
 
     }
 
+    /**
+     *
+     * The createGenericTransaction() method creates a new transaction given an
+     * InternalBlockBean, an InstanceWalletKeystoreInterface, and an integer
+     * index for the signKey. The method creates a new TransactionBean, sets its
+     * fields such as random seed, public key, message, and wallet cypher. It
+     * signs the message using the keyPairAtIndex method of the provided
+     * InstanceWalletKeystoreInterface. And returns the created transaction bean
+     * object.
+     *
+     * @param ibb - an InternalBlockBean object containing the details of the
+     * transaction
+     * @param iwk - an InstanceWalletKeystoreInterface containing the keypair
+     * used to sign the transaction
+     * @param signKey - an integer representing the index of the key used for
+     * signing the transaction
+     * @return - a TransactionBean object representing the created transaction
+     * @throws TransactionCanNotBeCreatedException - if an error occurred while
+     * creating the transaction
+     */
     public static TransactionBean createGenericTransaction(InternalBlockBean ibb, InstanceWalletKeystoreInterface iwk, int signKey) throws TransactionCanNotBeCreatedException {
         try {
             //todo verify itb transaction
@@ -137,15 +177,14 @@ public class TkmWallet {
     }
 
     /**
-     * It verifies the Transaction Bean content going through all the wrapped
-     * chain elements parsed
+     * This method is used to verify the {@link TransactionBean} content going
+     * through all the wrapped chain elements parsed. It uses the TkmCypherBean
+     * to check if the signature is valid.
      *
-     * use this function with caution, check for null values. For transaction
-     * integrity check use {@code verifyTransactionIntegrity}, for block
-     * integrity check use {@code BlockUtils.decodeBlock(String blockJson)}
-     *
-     * @param tb - Java Class Bean - Chrypted envelope
-     * @return TkmCypherBean
+     * @param tb the TransactionBean object containing the signature to be
+     * verified
+     * @return a TkmCypherBean object indicating if the signature is valid or
+     * not.
      */
     public static TkmCypherBean verifySign(TransactionBean tb) {
         TkmCypherBean tcb = null;
@@ -180,12 +219,46 @@ public class TkmWallet {
      * It creates a transaction box containing both Transaction bean and itb
      * message Only when the verifySign method sets a valid transaction
      *
-     * @param tb Java Class Bean - Chrypted envelope
-     * @param transactionJson
-     * @return
+     * Method to verify the integrity of a transaction, given the transaction
+     * bean and the transaction json.
+     *
+     * The method takes in two arguments: a TransactionBean object, which is an
+     * signed envelope containing a JSON string representation of a transaction,
+     * and a string representation of the same transaction in JSON format.
+     *
+     * @param tb the transaction bean
+     * @param transactionJson the json representation of the transaction
+     * @return a TransactionBox containing the result of the verification,
+     * including the Single Inclusion Transaction Hash, the TransactionBean, the
+     * InternalTransactionBean, the transactionJson and a boolean indicating if
+     * the transaction is valid
      */
     public static TransactionBox verifyTransactionIntegrity(TransactionBean tb, String transactionJson) {
+        /**
+         * The method first checks that the TransactionBean is not null. It then
+         * calls the verifySign method passing the TransactionBean object, to
+         * check the validity of the signature of the transaction. If the
+         * returned object is not null and is valid, it then checks the syntax
+         * of the transaction using the isTransactionBeanValid method and it
+         * also checks that the signer fields are valid using the verifySigner
+         * method, passed both the TransactionBean and an
+         * InternalTransactionBean object, which is created by deserializing the
+         * JSON string in the message field of the TransactionBean. If all the
+         * above checks are successful, it creates a inclusion hash and
+         * populates the TransactionBox object with the TransactionBean,
+         * InternalTransactionBean, the JSON string representation of the
+         * transaction and the inclusion hash. The validity of the
+         * TransactionBox is then set to true if the inclusion hash is
+         * successfully created. The verifyHashIntegrity is called passing the
+         * InternalTransactionBean and the TkmCypherBean object, to check the
+         * integrity of the hash in the transaction, and the validity of the
+         * TransactionBox object is set based on the returned result of this
+         * function call. If any of the above checks fail, the method will log
+         * an error message "INVALID!!" and return the TransactionBox object
+         * with the validity field set to false.
+         */
         TransactionBox result = new TransactionBox();
+        boolean inclusionHashCreation = false;
         if (tb != null) {
             TkmCypherBean cyBean = verifySign(tb);
             if (cyBean != null && cyBean.isValid()) {
@@ -206,12 +279,14 @@ public class TkmWallet {
                         result.setItb(itb);
                         result.setTransactionJson(transactionJson);
                         result.setValid(true);
+                        inclusionHashCreation = true;
                     } catch (InclusionHashCreationException ex) {
                         log.error("Transaction verification error", ex);
+                        inclusionHashCreation = false;
                     }
                 }
                 cyBean = verifyHashIntegrity(itb, cyBean);
-                result.setValid(cyBean.isValid() & signerFieldsVerification & transactionBeanValid.isValidSyntax());
+                result.setValid(cyBean.isValid() & signerFieldsVerification & transactionBeanValid.isValidSyntax() & inclusionHashCreation);
                 if (cyBean.getEx() != null) {
                     cyBean.getEx().printStackTrace();
                     log.error("INVALID!!");
@@ -226,6 +301,25 @@ public class TkmWallet {
         return result;
     }
 
+    /**
+     *
+     * This method is used to verify the integrity of the hash of an internal
+     * transaction bean (itb). It accepts an {@link InternalTransactionBean} and
+     * a {@link TkmCypherBean} as input and returns a {@link TkmCypherBean}. The
+     * method generates a hash of the itb and compares it with the
+     * 'transactionHash' field of the itb. If the hash generated is not equal to
+     * the transactionHash, the valid field of returned TkmCypherBean will be
+     * set to false and it will have exception message. This check is enforced
+     * for the transactions included in the main blockchain with a date greater
+     * than 1659534306000 (timestamp).
+     *
+     * @param itb is an internalTransactionBean for which integrity is to be
+     * checked
+     * @param cyBean is TkmCypherBean passed for maintaining previous exception
+     * messages if present.
+     * @return TkmCypherBean, where if the hash check passes it will be
+     * valid=true otherwise valid=false
+     */
     public static final TkmCypherBean verifyHashIntegrity(InternalTransactionBean itb, TkmCypherBean cyBean) {
         if (cyBean == null) {
             cyBean = new TkmCypherBean();
@@ -235,6 +329,11 @@ public class TkmWallet {
         try {
             String internalTransactionBeanHash = TkmTextUtils.internalTransactionBeanHash(itb);
             //enforce hash check after 1659534306000
+            //This check was introduced to account for some transactions 
+            //included in the main blockchain with an earlier date than 
+            //indicated, which due to a bug in the verifier, were accepted with 
+            //a hash format that did not comply with the specification.
+
             if (itb.getNotBefore().getTime() > 1659534306000L) {
                 if (!itb.getTransactionHash().equals(internalTransactionBeanHash)) {
                     cyBean.setValid(false);
